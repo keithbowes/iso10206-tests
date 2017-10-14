793ffced227113e37211400749c89e258a3d455a
Keith Bowes <keithbowes@users.noreply.github.com>
2017-10-14 05:33:51 -0400

    More number bases

--- numbase.pas
+++ numbase.pas
@@ -1,9 +1,7 @@
 program NumBase;
 
-var
-  a: integer;
-
 begin
-  a := 16#ff;
-  WriteLn(a);
+  WriteLn('There are ', 2#10, ' kinds of people');
+  WriteLn('Those who use strings limited to ', 16#ff, ' characters');
+  WriteLn('and those who like high-octal action movies: ', 8#77);
 end.
