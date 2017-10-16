969ab8ac872302fab86a50be2fa24b453e1c3b0a
Keith Bowes <keithbowes@users.noreply.github.com>
2017-10-16 02:43:32 -0400

    Any version of FPC you wish; the otherwise test is compiler agnostic

--- otherwise_test.pas
+++ otherwise_test.pas
@@ -1,11 +1,4 @@
 { Test out ISO 10206's new otherwise clause, which FPC 3.1.1's extendedpascal mode oddly omits (though, it has it in its default mode) }
-{$IFDEF FPC}
-{ "$IF FPCVERSION < 3.1" doesn't work for some reason }
-{$IF DEFINED(VER0) or DEFINED(VER1) or DEFINED(VER2) or DEFINED(VER3_0)}
-{$ERROR This is for testing FPC 3.1+'s support for otherwise in ISO 10206 mode}
-{$ENDIF}
-{$MODE extendedpascal}
-{$ENDIF}
 
 program Otherwise_Test(output);
 
