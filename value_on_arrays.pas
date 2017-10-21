ecff138c535db157e8303ab766c1c3c6dfb5ddc4
Keith Bowes <keithbowes@users.noreply.github.com>
2017-10-21 16:47:27 -0400

    Corrected and expanded value_on_arrays.pas

--- value_on_arrays.pas
+++ value_on_arrays.pas
@@ -3,12 +3,16 @@ program Value_on_Arrays(output);
 type
   MyArray = array[1..3] of integer value [1..2: 3; 3: 5];
   MyStrArray = array[1..3] of char value '*';
+  MyStrArray2 = array[1..3] of char value [1..3: '*'];
 
 var
   ma: MyArray;
   sa: MyStrArray;
+  s2: MyStrArray2;
 
 begin
-  WriteLn(ma[2]); {3}
-  WriteLn(sa[2]); {*}
+  WriteLn(ma[1], ':', ma[2], ':', ma[3]); {3:3:5}
+  WriteLn(sa); {*}
+  WriteLn(s2); {***}
+  WriteLn(s2[1], ':', s2[2], ':', s2[3]); {*:*:*}
 end.
