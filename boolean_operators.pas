77f8a0350593b4a55c9da86ac23610c4dbe12a7a
Keith Bowes <keithbowes@users.noreply.github.com>
2017-10-21 16:34:39 -0400

    Complete test for and_then and or_else

--- boolean_operators.pas
+++ boolean_operators.pas
@@ -1,6 +1,13 @@
 program Boolean_Operators(output);
 
 begin
-  WriteLn(true and_then not false);
-  WriteLn(true or_else false);
+  WriteLn('Should be true: ', true and_then not false);
+  WriteLn('Should be true: ', true and_then true);
+  WriteLn('Should be false: ', true and_then false);
+  WriteLn('Should be false: ', false and_then true);
+  WriteLn('Should be false: ', false and_then false);
+  WriteLn('Should be true: ', true or_else true);
+  WriteLn('Should be true: ', true or_else false);
+  WriteLn('Should be true: ', false or_else true);
+  WriteLn('Should be false: ', false or_else false);
 end.
