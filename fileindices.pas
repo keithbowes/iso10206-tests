2e72eaa0b25308def683f4c15c14e2f3e1126f30
Keith Bowes <keithbowes@users.noreply.github.com>
2019-04-12 12:21:22 -0400

    Added license

--- fileindices.pas
+++ fileindices.pas
@@ -1,11 +1,32 @@
 program FileIndices(f);
 
+{
+    ISO 10206 Tests
+    Copyright (C) 2017  Keith Bowes
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  }
+
 var
   b: BindingType;
   f: file[0..255] of char;
   i: 0..300;
 
 begin
+  Unbind(f);
+  b := Binding(f);
   b.Name := 'chartable';
   Bind(f, b);
   if Binding(f).Bound then
--- type_inquiry.pas
+++ type_inquiry.pas
@@ -1,5 +1,24 @@
 program Type_Inquiry(output);
 
+{
+    ISO 10206 Tests
+    Copyright (C) 2017  Keith Bowes
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+  }
+
 { I originally had b as a second parameter, but GPC failed, saying that a is undefined.
   Though, reading the standard, it seems that that should be allowed. }
 procedure p(a: integer);
